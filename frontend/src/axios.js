d1807c9a395c4c585ed4d93eb60338ee7c30c716
JASHoswal1234 <jashoswal269@gmail.com>
2026-02-28 10:29:20 +0530

    bug fixes

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,11 +1,10 @@
 import axios from 'axios';
 
 const API_URL = process.env.REACT_APP_BACKEND_URL || '';
-console.log(API_URL);
 
 const axiosInstance = axios.create({
   baseURL: API_URL,
-  // you can add other defaults here if needed
+  withCredentials: true,
 });
 
 export default axiosInstance;
